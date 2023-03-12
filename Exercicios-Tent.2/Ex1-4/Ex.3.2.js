ade948a40211f19ea3e9fb38600f3555caf35064
SuzanaAraujo <suoaraujo@gmail.com>
2023-03-12 18:19:01 -0300

    revisão e finalização

--- Exercicios-Tent.2/Ex1-4/Ex.3.2.js
+++ Exercicios-Tent.2/Ex1-4/Ex.3.2.js
@@ -1,16 +1,5 @@
-/* var marcasCadastradas = [];
-var modelosCadastrados = []; */
-var veiculosCadastrados = [];
-/* 
-function cadastrarMarca(nomeMarca) {
-  marcasCadastradas.push(nomeMarca);
-  console.log("Marca cadastrada com sucesso!");
-}
 
-function cadastrarModelo(nomeModelo) {
-  modelosCadastrados.push(nomeModelo);
-  console.log("Modelo cadastrado com sucesso!");
-} */
+var veiculosCadastrados = [];
 
 function cadastrarVeiculo(nomeMarca, nomeModelo, km, valor, cor) {
   veiculosCadastrados.push({ nomeMarca, nomeModelo, km, valor, cor });
@@ -21,6 +10,4 @@ cadastrarVeiculo("Volks", "Gol", 55000, 70000, "branco");
 cadastrarVeiculo("Chevrolet", "Saveiro", 5000, 50000, "cinza");
 cadastrarVeiculo("BMW", "X4", 55000, 200000, "azul");
 
-/* console.log(marcasCadastradas);
-console.log(modelosCadastrados); */
 console.log(veiculosCadastrados);
